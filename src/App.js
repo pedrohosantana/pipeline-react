b5f04b133a4e8354e00055171cf5d296daf06fee
pedrohosantana <pedrohosantana@gmail.com>
2023-09-11 17:08:44 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -71,9 +71,9 @@ export default function Game() {
   const moves = history.map((squares, move) => {
     let description;
     if (move > 0) {
-      description = 'Go to move #' + move;
+      description = '< Jogada #' + move;
     } else {
-      description = 'Go to game start';
+      description = 'Reiniciar Jogo';
     }
     return (
       <li key={move}>
